306b6525f2c26807d3e9914e5b8104aad3c10178
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-27 13:40:26 -0500

    feat: create routes

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,30 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+    const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+    app.route("/api/translate").post((req, res) => {
+        const { text, locale } = req.body;
+        if (text === undefined || locale === undefined) {
+            return res.json({ error: "Missing required field(s)" });
+        }
+        if (
+            locale !== "american-to-british" &&
+            locale !== "british-to-american"
+        ) {
+            return res.json({ error: "Invalid value for locale field" });
+        }
+        if (text === "") {
+            return res.json({ error: "No text to translate" });
+        }
+
+        const translation = translator.translate(text, locale);
+        if (text === translation) {
+            return res.json({ translation: "Everything looks good to me!" });
+        } else {
+            return res.json({ translation });
+        }
     });
 };
