e3a9f08d90befbb4638e16006bbf807d20b635e3
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-27 11:50:54 -0500

    feat: create British to American tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -114,4 +114,114 @@ suite("Unit Tests", () => {
             done();
         });
     });
+
+    suite("British to American Tests", () => {
+        test("Translate We watched the footie match for a while. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "We watched the footie match for a while.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "We watched the soccer match for a while."
+            );
+            done();
+        });
+        test("Translate Paracetamol takes up to an hour to work. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "Paracetamol takes up to an hour to work.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "Tylenol takes up to an hour to work."
+            );
+            done();
+        });
+        test("Translate First, caramelise the onions. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "First, caramelise the onions.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "First, caramelize the onions."
+            );
+            done();
+        });
+        test("Translate I spent the bank holiday at the funfair. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "I spent the bank holiday at the funfair.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "I spent the public holiday at the carnival"
+            );
+            done();
+        });
+        test("Translate I had a bicky then went to the chippy. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "I had a bicky then went to the chippy.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "I had a cookie then went to the fish-and-chip shop."
+            );
+            done();
+        });
+        test("Translate I've just got bits and bobs in my bum bag. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "I've just got bits and bobs in my bum bag.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "I've just got odds and ends in my fanny pack."
+            );
+            done();
+        });
+        test("Translate The car boot sale at Boxted Airfield was called off. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "The car boot sale at Boxted Airfield was called off.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "The swap meet at Boxted Airfield was called off."
+            );
+            done();
+        });
+        test("Translate Have you met Mrs Kalyani? to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "Have you met Mrs Kalyani?",
+                "british-to-american"
+            );
+            assert.strictEqual(temp.textContent, "Have you met Mrs. Kalyani?");
+            done();
+        });
+        test("Translate Prof Joyner of King's College, London. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "Prof Joyner of King's College, London.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "Prof. Joyner of King's College, London."
+            );
+            done();
+        });
+        test("Translate Tea time is usually around 4 or 4.30. to American English", (done) => {
+            temp.innerHTML = translate.translate(
+                "Tea time is usually around 4 or 4.30.",
+                "british-to-american"
+            );
+            assert.strictEqual(
+                temp.textContent,
+                "Tea time is usually around 4 or 4:30."
+            );
+            done();
+        });
+    });
 });
